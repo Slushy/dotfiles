dfedee13d4fa861c16d1f9ab9de5364f54038feb
Brandon Porter <brandon.porter@hulu.com>
2016-06-27 14:11:01 -0400

    Added install file

---
+++ install.sh
@@ -0,0 +1,5 @@
+# Install vim plugin manager
+if [[ ! -f ~/.config/nvim/autoload/plug.vim ]]; then
+		curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+				https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
