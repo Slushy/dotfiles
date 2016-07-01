7028380385acfb712a4d8fb6332c6e49896529d1
Brandon Porter <brandon.porter18@gmail.com>
2016-07-01 12:22:14 -0700

    Updated install file to link necessary files

--- install.sh
+++ install.sh
@@ -4,4 +4,26 @@ if [[ ! -f ~/.config/nvim/autoload/plug.vim ]]; then
 				https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-# TODO: Finish installation file
+# TODO: use CWD instead of hardcoded path
+# --------------------------------------------------
+# Create soft links
+# --------------------------------------------------
+
+: ${DOTFILE_DIR:=~/.dotfiles}
+: ${DOTFILE_LOCAL_DIR:=~/.dotfiles-local}
+
+[ ! -d $DOTFILE_LOCAL_DIR ] && mkdir $DOTFILE_LOCAL_DIR
+
+# ZSH
+ln -svf $DOTFILE_DIR/system/zshrc ~/.zshrc
+ln -svf $DOTFILE_DIR/system/zshenv ~/.zshenv
+
+# Vim
+ln -svf $DOTFILE_DIR/vim/init.vim ~/.config/nvim/init.vim
+ln -svf $DOTFILE_DIR/vim/settings.vim ~/.config/nvim/settings.vim
+
+# Git
+ln -svf $DOTFILE_DIR/git/gitconfig ~/.gitconfig
+
+# Tmux
+ln -svf $DOTFILE_DIR/tmux/tmux.conf ~/.tmux.conf
